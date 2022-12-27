07f16aa0a88cebc25e40f752fe11b22b2a1af2aa
user <user@DESKTOP-HFVGVI8>
2022-12-27 15:25:45 +0530

    Add project files.

---
+++ demoApp/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demoApp
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            // declaring list
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while (fibonacciNumbers.Count < 20) 
+            {
+                //selecting last two numbers in list
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous1 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                //adding the sum of last two numbers in list 
+                fibonacciNumbers.Add(previous + previous1);
+            }
+
+            //printing the items in the list
+            foreach(var item in fibonacciNumbers)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ demoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace demoApp
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            var names = new List<string> { "New friend","Scott","Kendra" };
+            names.Add("Maria");
+            names.Add("Bill");
+
+            names.Sort();
+            var index = names.IndexOf("Scott");
+            foreach(String name in names)
+            {
+                Console.WriteLine($"Hello {name} your index  is {names.IndexOf(name)}");
+            }
+        }
+    }
+}
